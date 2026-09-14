935a0116424945907e717ba0c590741b1df2d458
Dhruvaryan-10 <141823576+Dhruvaryan-10@users.noreply.github.com>
2026-09-14 11:21:41 +0530

    Time: 245 ms (43.36%), Space: 22.1 MB (11.89%) - LeetHub

---
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        freq = [0] * 26
+        for ch in s:
+            freq[ord(ch)- ord('a')] += 1
+        for ch in t:
+            freq[ord(ch) - ord('a')] -= 1
+        steps = 0
+        for count in freq:
+            steps += abs(count)
+        return steps
